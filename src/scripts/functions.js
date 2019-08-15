780e4de19aaf2348dc313020ede2c5e119dcd12d
Rohan Mahajan <rohan.mahajan@gartner.com>
2019-08-15 16:49:44 +0530

    new changes

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -1,13 +1,16 @@
 let totalCount = 0,
     itemsInArray = [],
-    itemsObj = {};
+    itemsObj = {},
+    finalArray =[];
 
 const products = document.querySelector('.product-table');
+const buyNow = document.querySelector('.js-buy');
+
 
 const addMinus = (event, value) => {
     let $objectElem = $(event).parents('.js-item-row'),
         obejItem = $objectElem.find('.js-item').text(),
-        totalValue = parseFloat($objectElem.find('.js-cost-in').text()) * value;
+        totalValue = (parseFloat($objectElem.find('.js-cost-in').text()) * value).toFixed(2) ;
     $objectElem.find('.js-cost-final').text(totalValue);
     itemsObj[obejItem] = value;
     Object.keys(itemsObj).forEach(function (index, value) {
@@ -16,7 +19,7 @@ const addMinus = (event, value) => {
         itemsInArray.push(itemToPush);
     })
 
-    console.log(itemsInArray);
+    finalArray = Array.from(itemsInArray);
     itemsInArray = [];
 }
 
@@ -29,12 +32,22 @@ const updateTotals = () => {
     console.log(itemsCost);
     let sum = 0;
     [...itemsCost].forEach(item => {
-        sum += parseFloat(item.innerHTML);
+        sum += (parseFloat(item.innerHTML)).toFixed(2);
     });
 
-    subTotalText.innerHTML = sum;
-    vatText.innerHTML = sum * 0.2;
-    totalText.innerHTML = sum + sum * 0.2;
+    subTotalText.innerHTML = (parseFloat(sum)).toFixed(2);
+    vatText.innerHTML = (parseFloat(sum) * 0.2).toFixed(2);
+    totalText.innerHTML = (parseFloat(sum + sum) * 0.2).toFixed(2);
+}
+const commonFunction = (target,value) => {
+    if(value <11) {
+        addMinus(target,value);
+        updateTotals();
+    }
+    else {
+        $(target).parents('.js-item-row').find('.js-input-value').val(10);
+    }
+    
 }
 
 //=================================================================
@@ -46,8 +59,8 @@ $(document).on('click', '.js-minus', function (e) {
     let $objectElem = $(e.currentTarget).parents('.js-item-row'),
         $inputElem = $objectElem.find('.js-input-value');
     $inputElem.val(parseInt($inputElem.val()) - 1);
-    addMinus(e.currentTarget, parseInt($inputElem.val()));
-    updateTotals();
+    commonFunction(e.currentTarget, parseInt($inputElem.val()));
+    
 });
 
 $(document).on('click', '.js-plus', function (e) {
@@ -55,14 +68,13 @@ $(document).on('click', '.js-plus', function (e) {
     let $objectElem = $(e.currentTarget).parents('.js-item-row'),
         $inputElem = $objectElem.find('.js-input-value');
     $inputElem.val(parseInt($inputElem.val()) + 1);
-    addMinus(e.currentTarget, parseInt($inputElem.val()));
-    updateTotals();
+    commonFunction(e.currentTarget, parseInt($inputElem.val()));
+    
 });
 
 $(document).on('change', '.js-input-value', function (e) {
     if ($(e.currentTarget).val()) {
-        addMinus(e.currentTarget, parseInt($(e.currentTarget).val()));
-        updateTotals();
+        commonFunction(e.currentTarget, parseInt($(e.currentTarget).val()));
     }
 
 })
@@ -76,7 +88,26 @@ products.addEventListener('click', function (event) {
     if (!event.target.matches('.js-trash')) return;
     let row = event.target.parentNode.parentNode;
     row.parentNode.removeChild(row);
+    if(document.querySelectorAll('.js-item-row').length < 1) {
+        buyNow.setAttribute('disabled','disabled');
+    }
     updateTotals();
 }, false);
+buyNow.addEventListener('click',function(event){
+
+$.ajax({
+    type:'POST',
+    url:'#',
+    data:finalArray,
+    dataType:'json',
+    success:function(resultData) {
+        console.log('data posted'+ resultData);
+    },
+    error:function(error) {
+        console.log('error issue'+error.responseText);
+        console.log('data posted'+ resultData);
+    }
+})
+},false);
 
 export {updateTotals, addMinus};
